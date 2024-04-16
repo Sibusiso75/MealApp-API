49efb14ae2087b135090730145ca08290cee2b17
Sibusiso75 <sibusisomatebese75@gmail.com>
2024-04-16 15:20:14 +0200

    changes url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://s-mealrecipes.netlify.app"],
     credentials:true
 }))
 app.use(cookieParser())
