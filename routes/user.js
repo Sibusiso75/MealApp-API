189642497c536a18d334fd4332df05a337d0baa6
Sibusiso75 <sibusisomatebese75@gmail.com>
2024-04-20 04:47:27 +0200

    add token.js

--- routes/user.js
+++ routes/user.js
@@ -4,46 +4,131 @@ const jwt = require("jsonwebtoken")
 const nodemailer = require("nodemailer")
 const User= require("../models/User.js")
 const router = express.Router()
+const Token = require("../models/Token.js")
+// const crypto = require("crypto")
 
 router.get("/", (req, res)=>{
   res.send("Hello world from the server")
 })
+
 router.post("/register", async (req, res)=>{
     const {username, email, password} = req.body;
-    const user = await User.findOne({email})
+    let user = await User.findOne({email})
     if(user){
       return res.json({message:"User already exists"})
     }
     const hashedPassword = await bcrypt.hash(password, 10)
-    const newUser = new User({
+
+    user = new User({
       username,
       email,
       password:hashedPassword,
     })
-    await newUser.save()
-  return res.json({status:true,message:"user registered"})
+    await user.save()
+    const token = new Token({
+      userId:user._id,
+      token:crypto.randomBytes(32).toString("hex")
+    })
+    await token.save()
+    var transporter = nodemailer.createTransport({
+      service:"gmail",
+      auth:{
+        user:process.env.MYEMAIL, pass:process.env.MEALPASS}
+    })
+    var mailOptions = {
+      from:process.env.MYEMAIL,
+      to:email,
+      subject:"Verify Email",
+      html:`<p>Hi, ${user.username}</p>
+      <p>Verify your account by clicking the link below</p>       
+      <a href="https://s-mealrecipes.netlify.app/users/${user_id}/verify/${token.token}">
+      Verify link</a>
+      `
+    }
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+        console.log(error)
+      }
+      else{
+       return res.json({message:"Email sent"})
+      }
+    })
+
+  return res.json({status:true,message:"Verification link is sent to your email verify your account. Please check it."})
 })
+router.get("/:id/verify/:token", async (req, res)=>{
+  try {
+    const user = await User.findOne({_id:req.params.id})
+    if(!user){
+      return res.status(400).send({message:"Invalid link"})
+    }
+    const token = await Token.findOne({
+      userId:user_id,
+      token:req.params.token,
+    })
+    if(!token){
+      return res.status(400).send({message:"Invalid link"})
+    }
+    await User.updateOne({_id:user._id, verified:true})
+    await token.remove()
+    res.status(200).send({message:"Email verified successfully"})
+  } catch (error) {
+    res.status(500).send({message:"Internal server error"})
+
+  }
+})
+
+
 router.post("/login", async(req, res)=>{
    const {email, password} = req.body;
    const user = await User.findOne({email})
    if(!user){
   return res.json({message:"User does not exist"})
-   }
+}
    const passwordValid = await bcrypt.compare(password, user.password)
 
    if(!passwordValid){
     return res.json({message:"Password is incorrect"})
    }
-  
+   if(!user.verified){
+    let token = await Token.findOne({userId:user_id})
+    if(!token){
+      var transporter = nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+          user:process.env.MYEMAIL, pass:process.env.MEALPASS}
+      })
+      var mailOptions = {
+        from:process.env.MYEMAIL,
+        to:email,
+        subject:"Verify Email",
+        html:`<p>Hi, ${user.username}</p>
+        <p>Verify your account by clicking the link below</p>       
+        <a href="https://s-mealrecipes.netlify.app/users/${user_id}/verify/${token.token}">
+        Verify link</a>
+        `
+      }
+      transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+          console.log(error)
+        }
+        else{
+         return res.json({message:"Email sent"})
+        }
+      })
+    }
+    return res.json({status:true})
+   }
    const token = jwt.sign({
-    username:user.username,
-   }, process.env.KEY, {expiresIn:"1h"})
-   res.cookie('token', token, {httpOnly:true,sameSite:"none",secure:true, maxAge:360000})
-    return res.json({status:true, message:"User logged in successfully"})
-})
+    username:user.username }, process.env.KEY, {expiresIn:"1h"})
+
+   res.cookie('token', token, 
+   {httpOnly:true,sameSite:"none",secure:true, maxAge:360000})
+    return res.json({status:true, message:"User logged in successfully"})})
+
 
 router.post("/forgot-password", async (req, res)=>{
-  const {email} = req.body;
+  const {email} = req.body;  
   try {
     const user =  await User.findOne({email})
     if(!user){
@@ -93,18 +178,18 @@ router.post("/reset-password/:token", async (req, res)=>{
      return res.json("Invalid token")}
 })
 
-const verifyUser = async(req, res)=>{
-try {
-  const token = req.cookies.token;
-  if(!token){
-    return res.json({status:false, message:"no token valid"})
+async function verifyUser(req, res) {
+  try {
+    const token = req.cookies.token
+    if (!token) {
+      return res.json({ status: false, message: "no token valid" })
+    }
+    const decoded = jwt.verify(token, process.env.KEY)
+    const username = decoded.username
+    return res.json({ status: true, username: username })
+  } catch (error) {
+    return res.json(error)
   }
-  const decoded = jwt.verify(token, process.env.KEY)
-  const username= decoded.username;
-  return res.json({status:true, username:username})
-} catch (error) {
-  return res.json(error)
-}
 }
 
 router.get("/verify",verifyUser, async (req, res)=>{
@@ -117,61 +202,5 @@ router.get("/logout", async (req, res)=>{
 })
 
 module.exports = router 
-// const router = require("express").Router()
-// const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken")
-// const User = require("../models/User")
-// const bcrypt = require("bcryptjs")
-// // const path = require("path")
-// // const multer = require("multer")
-
-// // const storage = multer.diskStorage({
-// //   destination:(req, file, cb)=>{
-// //   cb(null, "public/Images")
-// //   },
-// //   filename:(req, file, cb)=>{
-// //       cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
-// //       }
-
-// // })
-
-// // const upload = multer({
-// //   storage:storage
-// // })
-// // router.post("/upload", upload.single("file"),(req, res)=>{
- 
-// // })
 
 
-// //UPDATE - add verifyTokenAndAuthorization
-// router.put("/:id",  async (req, res)=>{    
-//      try {
-//           const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//         const updatedUser = await User.findByIdAndUpdate(req.params.id,{
-//             $set:req.body, password:hashedPassword
-//         },{new:true})
-//         res.status(200).json(updatedUser)
-//      } catch (error) {
-//         res.status(500).json(error)
-//      }
-// })
-// //DELETE - add verifyTokenAndAuthorization
-
-
-// router.delete("/:id", async(req, res)=>{
-//   try {
-//     await User.findByIdAndDelete(req.params.id)
-//   } catch (error) {
-//     res.status(500).json(error)
-//   }
-// })
-// // GET - add verifyTokenAndAdmin
-// router.get("/", async(req, res)=>{
-//     try {
-//      const users = await User.find();
-//      res.status(200).json(users)
-//     } catch (error) {
-//       res.status(500).json(error)
-//     }
-//   })
-
-// module.exports = router
